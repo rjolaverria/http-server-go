d72e4ff213bfe8467e8b3693e0d005f4f820e780
rjolaverria <rjolaverria@gmail.com>
2025-03-15 17:04:19 -0400

    respond with 200

--- app/main.go
+++ app/main.go
@@ -6,15 +6,7 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -22,9 +14,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	response := []byte("HTTP/1.1 200 OK\r\n\r\n")
+
+	writeResponse(conn, response)
+}
+
+func writeResponse(conn net.Conn, response []byte) {
+	_, err := conn.Write(response)
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+	}
+	conn.Close()
 }
