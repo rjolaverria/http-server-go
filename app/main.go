f4c02b9a9120d02cbc53ca63b876492cf356a8d6
rjolaverria <rjolaverria@gmail.com>
2025-03-16 12:48:11 -0400

    respond with body

--- app/main.go
+++ app/main.go
@@ -24,9 +24,13 @@ func main() {
 		return response.NewResponse(response.OK, nil, nil)
 	})
 	router.GET("/echo/{str}", func(req *request.Request) *response.Response {
-		return response.NewResponse(response.OK, nil, []byte(req.PathParams["str"]))
+		headers := make(map[string]string)
+		headers["Content-Type"] = "text/plain"
+		headers["Content-Length"] = fmt.Sprintf("%d", len(req.PathParams["str"]))
+		return response.NewResponse(response.OK, headers, []byte(req.PathParams["str"]))
 	})
 	router.GET("/echo/{str}/more/{str2}", func(req *request.Request) *response.Response {
+
 		return response.NewResponse(response.OK, nil, []byte(req.PathParams["str"]+req.PathParams["str2"]))
 	})
 
